9033ba69ff8649909cb939bf687ca102d686e7cd
Landon Reasoner <reasoner.landon90@gmail.com>
2020-11-08 01:36:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
   
-  attr_accessor :sender, :receiver, :amount, :status
+  attr_accessor :sender, :receiver, :amount, :status, :count
   
   def initialize(sender, receiver, amount)
     @sender = sender
