6a9018db606af160f8804a4e31f2cd5a7a699b13
Landon Reasoner <reasoner.landon90@gmail.com>
2020-11-08 01:26:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
     elsif @status == "complete"
       puts "Transaction was already executed"
     else 
-      @sender.deposit(amount * -1)
-      @receiver.deposit(amount )
+      @sender.deposit(@amount * -1)
+      @receiver.deposit(@amount)
   end 
 end
